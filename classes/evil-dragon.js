5d7f8175c2c09c598c803af1cd037a87108d2f3b
Alex Hiller <asonnyhiller@gmail.com>
2022-03-04 00:07:36 -0500

    seriously go watch homestarrunner

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,6 +1,26 @@
 const Dragon = require("./dragon")
 
+class EvilDragon extends Dragon{
+    constructor(name, color, evilDoings, nemesis){
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
 
+    dontInviteThemOverForDinner(){
+        this.evilDoings.forEach(evilDoing => console.log(`${this.name} will ${evilDoing}`));
+    }
+
+    burnsNemesis(){
+        return `${this.name} destroys ${this.nemesis} with fire! WOOOSH!!!`;
+    }
+
+}
+
+const TROGDOR = new EvilDragon("TROGDOOOR!!!", "green", ["burninate the countryside", "burninate all the peasants"], "the peasants");
+
+TROGDOR.dontInviteThemOverForDinner();
+console.log(TROGDOR.burnsNemesis());
 
 try {
     module.exports = EvilDragon;
